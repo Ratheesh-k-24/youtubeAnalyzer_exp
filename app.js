b2a12c57169ac9cfb6767d0db72411f559ffef83
Ratheesh-k-24 <ratheeshkarnan007@gmail.com>
2024-03-13 10:30:38 +0530

    added body-parser

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const mongooseConnection = require("./model/db");
 const app = express();
 const path = require("path");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 const userRoutes = require("./routes/userRoutes");
 const videosRoutes = require("./routes/videosRoutes");
 
 app.use(express.json());
 app.use(cors());
+app.use(bodyParser.json());
 app.use(express.static("views"));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
